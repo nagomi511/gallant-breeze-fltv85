c2750a413ba24238b22b46a0abb16126e77806a3
nagomi <125933959+nagomi511@users.noreply.github.com>
2025-11-28 08:01:09 +0000

    fix

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -3,36 +3,44 @@ import "./styles.css";
 export const Todo = () => {
   return (
   <>
-  <div>
+  <div className="input-area">
     <input placeholder="Todoを入力" />
     <button>追加</button>
   </div>
-  <div>
-    <p>未完了のTODO</p>
+  <div className="complete-area">
+    <p className="title">未完了のTODO</p>
     <ul>
       <li>
+        <div className="list-row">
         <p>TODOdesu</p>
         <button>done</button>
         <button>delete</button>
+        </div>
       </li>
       <li>
+        <div className="list-row">
         <p>TODOdesu</p>
         <button>done</button>
         <button>delete</button>
+        </div>
       </li>
     </ul>
   </div>
 
-  <div>
-    <p>完了のTODO</p>
+  <div className="incomplete-area">
+    <p className="title">完了のTODO</p>
     <ul>
       <li>
+      <div className="list-row">
         <p>TODOdesu</p>
         <button>return</button>
+      </div>
       </li>
       <li>
+      <div className="list-row">
         <p>TODOdesu</p>
         <button>return</button>
+      </div>
       </li>
     </ul>
   </div>
