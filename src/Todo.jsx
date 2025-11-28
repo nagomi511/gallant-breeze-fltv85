b66c931bdb8b739f842474a32bfad267cbcae69d
nagomi <125933959+nagomi511@users.noreply.github.com>
2025-11-28 21:08:58 +0000

    add map

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -3,50 +3,48 @@ import "./styles.css";
 import { createIncrementalCompilerHost } from "typescript";
 
 export const Todo = () => {
-  const [incompleteTodos, setImcommpleteTodos] = useState();
+  const [incompleteTodos, setImcommpleteTodos] = useState([
+    "TODO1desu", 
+    "TODO2desu"
+  ]);
+  const [completeTodos, setcommpleteTodos] = useState(
+    ["TODO1desita",
+     "TODO2desita"
+    ]);
   return (
-  <>
-  <div className="input-area">
-    <input placeholder="Todoを入力" />
-    <button>追加</button>
-  </div>
-  <div className="complete-area">
-    <p className="title">未完了のTODO</p>
-    <ul>
-      <li>
-        <div className="list-row">
-        <p>TODOdesu</p>
-        <button>done</button>
-        <button>delete</button>
-        </div>
-      </li>
-      <li>
-        <div className="list-row">
-        <p>TODOdesu</p>
-        <button>done</button>
-        <button>delete</button>
-        </div>
-      </li>
-    </ul>
-  </div>
+    <>
+      <div className="input-area">
+        <input placeholder="Todoを入力" />
+        <button>追加</button>
+      </div>
+      <div className="complete-area">
+        <p className="title">未完了のTODO</p>
+        <ul>
+          {incompleteTodos.map((todo) => (
+            <li key={todo}>
+              <div className="list-row">
+                <p claaName="todo-item">{todo}</p>
+                <button>done</button>
+                <button>delete</button>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
 
-  <div className="incomplete-area">
-    <p className="title">完了のTODO</p>
-    <ul>
-      <li>
-      <div className="list-row">
-        <p>TODOdesita</p>
-        <button>return</button>
+      <div className="incomplete-area">
+        <p className="title">完了のTODO</p>
+        <ul>
+          {completeTodos.map((todo) => (
+            <li key={todo}>
+              <div className="list-row">
+                <p className="todo-item">{todo}</p>
+                <button>return</button>
+              </div>
+            </li>
+          ))}
+        </ul>
       </div>
-      </li>
-      <li>
-      <div className="list-row">
-        <p>TODOdesita</p>
-        <button>return</button>
-      </div>
-      </li>
-    </ul>
-  </div>
-  </>
+    </>
   );
 };
