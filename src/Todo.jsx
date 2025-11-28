43570da97f83dce9100102c31ef13444f60ea5f9
nagomi <125933959+nagomi511@users.noreply.github.com>
2025-11-28 07:29:34 +0000

    fix

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -1,5 +1,41 @@
 import "./styles.css";
 
 export const Todo = () => {
-  return <div>hoge</div>;
+  return (
+  <>
+  <div>
+    <input placeholder="Todoを入力" />
+    <button>追加</button>
+  </div>
+  <div>
+    <p>未完了のTODO</p>
+    <ul>
+      <li>
+        <p>TODOdesu</p>
+        <button>done</button>
+        <button>delete</button>
+      </li>
+      <li>
+        <p>TODOdesu</p>
+        <button>done</button>
+        <button>delete</button>
+      </li>
+    </ul>
+  </div>
+
+  <div>
+    <p>完了のTODO</p>
+    <ul>
+      <li>
+        <p>TODOdesu</p>
+        <button>return</button>
+      </li>
+      <li>
+        <p>TODOdesu</p>
+        <button>return</button>
+      </li>
+    </ul>
+  </div>
+  </>
+  );
 };
